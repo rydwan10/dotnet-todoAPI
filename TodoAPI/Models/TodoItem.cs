0f10f97530f77f8d69d5a21da22a0b9be5b5a1f8
Muhammad Rydwan <rydwan.dev@gmail.com>
2021-04-11 21:41:26 +0700

    Add project files.

---
+++ TodoAPI/Models/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoAPI.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public String Name { get; set; }
+        public bool IsComplete { get; set; }
+
+    }
+}
